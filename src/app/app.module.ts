0ad39a2fbe73ce35e3bd5077af5068e6594ed45c
antonio-bit-1109 <antonio.rizzuti@hotmail.com>
2025-01-08 18:49:41 +0100

    add login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DividerModule } from 'primeng/divider';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule ,
+    BrowserAnimationsModule , 
+    DividerModule ,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
